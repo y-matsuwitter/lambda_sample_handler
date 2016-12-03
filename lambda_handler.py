36cfab3560955f34f1783589c102aa1e55cb6131
y-matsuwitter <info@yuki-matsumoto.com>
2016-12-03 19:00:11 +0900

    Add print

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,6 +1,8 @@
 #!-*-coding:utf-8-*-
+from __future__ import print_function
 import requests
 
 def lambda_handler(event, hoge):
+    print("check status_code")
     response = requests.get("https://gunosy.com")
     return response.status_code
